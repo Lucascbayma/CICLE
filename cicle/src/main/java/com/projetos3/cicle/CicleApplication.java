bd11db85caeca3f0a9fb23b2b3afaa14c659710f
Lucascbayma <Lucasbayma31@gmail.com>
2026-05-05 12:10:05 -0300

    Adicionando Spring boot ao projeto!

---
+++ cicle/src/main/java/com/projetos3/cicle/CicleApplication.java
@@ -0,0 +1,13 @@
+package com.projetos3.cicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicleApplication.class, args);
+    }
+
+}
---
+++ cicle/src/test/java/com/projetos3/cicle/CicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos3.cicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
